990f36dbc09522dc5f0c63a21c962610cbddb4a6
Antony John Kirubagaran <antonykirubagaran4444@gmail.com>
2026-03-04 11:04:32 +0530

    Auto-commit before pushing to GitHub

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -18,10 +18,9 @@ const userSchema: Schema = new Schema({
     timestamps: true,
 });
 
-// Hash password before saving
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password as string, salt);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://healthyfy-0hkm.onrender.com',
+    baseURL: 'http://localhost:5000/api',
 });
 
 // Add a request interceptor to include JWT token
